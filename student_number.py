593bf6607f992f4efe2ee9d3bbfcb3949fdf05ed
Louis Augey <louis.augey@orange.fr>
2024-05-31 14:01:52 +0200

    Update student_number.py

--- student_number.py
+++ student_number.py
@@ -2,9 +2,11 @@ from random import randint
 
 def generate_student_number():
     student_member_list = []
-    for i in range(1000):
-        student_number = str(randint(2230, 2330)) + ''.join([str(randint(0, 9)) for j in range(4)])
-        if student_number not in student_member_list:
-            student_member_list.append(student_number)
+    
+    student_number = randint(22300000,23300000)
+    if student_number not in student_member_list:
+        student_member_list.append(student_number)
+    else : 
+        generate_student_number()
     return student_member_list
 print(generate_student_number())
