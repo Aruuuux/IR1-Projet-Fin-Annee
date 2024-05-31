77a48bfa7ed34da86da98f4e49d3cee869790ac4
hamzabaya <bayahamza1010@gmail.com>
2024-05-31 15:13:01 +0200

    anglais

--- Django/projet/databaseprojet/models.py
+++ Django/projet/databaseprojet/models.py
@@ -4,33 +4,33 @@ from django.db import models
 from django.db import models
 
 # Statut
-class Statut(models.Model):
-    STATUT_CHOICES = [
+class Status(models.Model):
+    STATUS_CHOICES = [
         (1, 'Student'),
         (2, 'Teacher'),
         (3, 'Supervisor'),
     ]
     id = models.AutoField(primary_key=True)
-    name = models.IntegerField(choices=STATUT_CHOICES, unique=True)
+    name = models.IntegerField(choices=STATUS_CHOICES, unique=True)
 
     def __str__(self):
         return self.get_name_display()
 
-class Specialite(models.Model):
-    SPECIALITE_CHOICES = [
-        (1, 'IR'),
-        (2, 'ASE'),
+class Speciality(models.Model):
+    SPECIALITY_CHOICES = [
+        (1, 'Informatique et réseaux'),
+        (2, 'Automatique et systèmes embarqués'),
         (3, 'Textile'),
         (4, 'Mécanique'),
         (5, 'Génie industriel'),
     ]
     id = models.AutoField(primary_key=True)
-    specialite_id = models.IntegerField(choices=SPECIALITE_CHOICES, unique=True)
+    speciality_id = models.IntegerField(choices=SPECIALITY_CHOICES, unique=True)
     name = models.CharField(max_length=255)
     responsible_id = models.IntegerField() 
 
     def __str__(self):
-        return dict(self.SPECIALITE_CHOICES).get(self.specialite_id, 'Unknown')
+        return dict(self.SPECIALITY_CHOICES).get(self.speciality_id, 'Unknown')
 
 class MyModel(models.Model):
     name = models.CharField(max_length=100)
