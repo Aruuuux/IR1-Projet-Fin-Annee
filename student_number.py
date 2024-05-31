8f915f0cb4af50dd633642637ac6525499a07e0b
Louis Augey <louis.augey@orange.fr>
2024-05-31 13:37:24 +0200

    update generateur numero etudiant
    rend les numéros uniques et plus conforme aux numéros étudiants

--- student_number.py
+++ student_number.py
@@ -1,8 +1,10 @@
-from random import *
+from random import randint
 
 def generate_student_number():
-    student_number = "22"
-    for i in range(6):
-        student_number += str(randint(0, 9))
-    return student_number
+    student_member_list = []
+    for i in range(1000):
+        student_number = str(randint(2230, 2330)) + ''.join([str(randint(0, 9)) for j in range(4)])
+        if student_number not in student_member_list:
+            student_member_list.append(student_number)
+    return student_member_list
 print(generate_student_number())
