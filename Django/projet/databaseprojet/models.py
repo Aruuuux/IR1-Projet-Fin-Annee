6f4ee88fe72739af8ede1fc2d6c6a7904906be13
hamzabaya <bayahamza1010@gmail.com>
2024-05-31 15:52:17 +0200

    User

--- Django/projet/databaseprojet/models.py
+++ Django/projet/databaseprojet/models.py
@@ -1,13 +1,8 @@
 from django.db import models
+import re
+from django.databaseprojet.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 
-
-
-class MyModel(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-
-    def __str__(self):
-        return self.name
 
 class Course(models.Model):
     id=models.AutoField(primary_key=True)
@@ -22,7 +17,7 @@ class Course(models.Model):
     coefficient_PW=models.IntegerField() 
     coefficient_DW=models.IntegerField() 
 
-# Statut
+
 class Status(models.Model):
     STATUS_CHOICES = [
         (1, 'Student'),
@@ -34,6 +29,7 @@ class Status(models.Model):
 
     def __str__(self):
         return self.get_name_display()
+
 
 class Speciality(models.Model):
     SPECIALITY_CHOICES = [
@@ -50,6 +46,31 @@ class Speciality(models.Model):
 
     def __str__(self):
         return dict(self.SPECIALITY_CHOICES).get(self.speciality_id, 'Unknown')
+
+
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    roles = models.ForeignKey(Roles, on_delete=models.CASCADE)
+    date_of_birth = models.DateField()
+    speciality = models.ForeignKey(Speciality, on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to='photos/')
+    email = models.EmailField(unique=True)
+    password = models.TextField(max_length=255)
+    student_id = models.IntegerField(max_length=50, unique=True)
+    year = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+    
+    def clean(self):
+        super().clean()
+        email_pattern = '^[a-zA-Z]+\.[a-zA-Z]+@uha\.fr$'
+        if not re.match(email_pattern, self.email):
+            raise ValidationError(_('Email doit être sous la forme prenom.nom@uha.fr'))
+
+
 
 class MyModel(models.Model):
     name = models.CharField(max_length=100)
