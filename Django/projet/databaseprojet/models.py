45f8e5aa99227336f37d011a73956b7d25c85fb8
hamzabaya <bayahamza1010@gmail.com>
2024-05-31 14:54:26 +0200

    statut

--- Django/projet/databaseprojet/models.py
+++ Django/projet/databaseprojet/models.py
@@ -1,5 +1,22 @@
 from django.db import models
 
+
+from django.db import models
+
+# Statut
+class Statut(models.Model):
+    STATUT_CHOICES = [
+        (1, 'Student'),
+        (2, 'Teacher'),
+        (3, 'Supervisor'),
+    ]
+    id = models.AutoField(primary_key=True)
+    name = models.IntegerField(choices=STATUT_CHOICES, unique=True)
+
+    def __str__(self):
+        return self.get_name_display()
+
+
 class MyModel(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
