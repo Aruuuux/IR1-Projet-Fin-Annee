8c50df5976ccc6eac9b5d5722b3ff1f5aa0e768c
hamzabaya <bayahamza1010@gmail.com>
2024-05-31 15:02:40 +0200

    specialité

--- Django/projet/databaseprojet/models.py
+++ Django/projet/databaseprojet/models.py
@@ -16,6 +16,21 @@ class Statut(models.Model):
     def __str__(self):
         return self.get_name_display()
 
+class Specialite(models.Model):
+    SPECIALITE_CHOICES = [
+        (1, 'IR'),
+        (2, 'ASE'),
+        (3, 'Textile'),
+        (4, 'Mécanique'),
+        (5, 'Génie industriel'),
+    ]
+    id = models.AutoField(primary_key=True)
+    specialite_id = models.IntegerField(choices=SPECIALITE_CHOICES, unique=True)
+    name = models.CharField(max_length=255)
+    responsible_id = models.IntegerField() 
+
+    def __str__(self):
+        return dict(self.SPECIALITE_CHOICES).get(self.specialite_id, 'Unknown')
 
 class MyModel(models.Model):
     name = models.CharField(max_length=100)
