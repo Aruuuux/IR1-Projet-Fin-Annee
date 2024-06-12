56e7a212016ec7dad1d33e965821a6429ef136e9
Louis Augey <louis.augey@orange.fr>
2024-06-12 14:28:50 +0200

    replace icone AF for svg file

--- Django/projet/projet/settings.py
+++ Django/projet/projet/settings.py
@@ -43,10 +43,9 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     'databaseprojet',
     'user',
-    'module'
-    # 'guardian', #Pour la gestion des permissions
+    'module'   
 ]
-
+# 'guardian', #Pour la gestion des permissions
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',  
     #'guardian.backends.ObjectPermissionBackend',
